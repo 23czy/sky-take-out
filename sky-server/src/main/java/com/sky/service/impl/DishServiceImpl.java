12241afac90eb2bebcf02d81515595c5852d9f7b
23czy <2269234696@qq.com>
2026-06-13 19:48:45 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -27,6 +27,50 @@ import java.util.List;
 @Slf4j
 public class DishServiceImpl implements DishService {
     /***
+     * 根据id修改菜品基本信息和对应口味信息
+     * @param dishDTO
+     */
+    @Override
+    public void updateWithFlavor(DishDTO dishDTO) {
+        //修改菜品表基本信息
+        Dish dish=new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        dishMapper.update(dish);
+
+        //删除原有的口味数据
+        dishFlavorMapper.deleteByDishId(dish.getId());
+
+        //重新插入口味数据
+        List<DishFlavor> flavors=dishDTO.getFlavors();
+        if(flavors!=null&&flavors.size()>0){
+             flavors.forEach(dishFlavor->{
+                 dishFlavor.setDishId(dishDTO.getId());
+             });
+             //向口味表插入多条数据
+             dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+
+    /***
+     * 根据id查询对应菜品和口味数据
+     * @param id
+     * @return
+     */
+    @Override
+    public DishVO getByIdWithFlavor(Long id) {
+        //根据id查询菜品数据
+        Dish dish=dishMapper.getById(id);
+
+        //根据id查询口味数据
+        List<DishFlavor> dishFlavors= dishFlavorMapper.getByDishId(id);
+        //将查询到的数据封装到VO
+        DishVO dishVO=new DishVO();
+        BeanUtils.copyProperties(dish,dishVO);
+        dishVO.setFlavors(dishFlavors);
+        return dishVO;
+    }
+
+    /***
      * 菜品批量删除
      * @param ids
      */
@@ -49,12 +93,16 @@ public class DishServiceImpl implements DishService {
             throw new DeletionNotAllowedException(MessageConstant.CATEGORY_BE_RELATED_BY_DISH);
         }
         //删除菜品表数据
-        for(Long id:ids){
+        /*for(Long id:ids){
             dishMapper.deleteById(id);
             //删除菜品关联的口味数据
             dishFlavorMapper.deleteByDishId(id);
         }
-
+        */
+        //根据菜品id集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+        //根据菜品id集合批量删除关联的口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
     }
 
     /***

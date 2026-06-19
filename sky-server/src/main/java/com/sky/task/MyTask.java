c22d76976be7c045683e4b767fff42efe2d89eb9
23czy <2269234696@qq.com>
2026-06-19 20:21:43 +0800

    订单状态定时处理业务代码开发

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,17 @@
+package com.sky.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/***
+ * 自定义定时任务类
+ */
+@Component
+@Slf4j
+public class MyTask {
+
+    /***
+     * 定时任务 每隔5秒触发一次
+     */
+
+}

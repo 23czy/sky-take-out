c6b1b396def4c44a1e3164b57b8ad52ac1874f05
23czy <2269234696@qq.com>
2026-06-20 11:31:49 +0800

    营业额与用户数据统计业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import com.github.pagehelper.Page;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -57,4 +58,11 @@ public interface OrderMapper {
      */
     @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime  orderTime);
+
+    /***
+     * 根据被动态条件统计营业额数据
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map<String, Object> map);
 }
